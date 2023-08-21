ae9af59b605264ec8ed10c0704ba0c3677eedca7
Katia Nienow <nienowkatia@gmail.com>
2023-08-21 00:28:09 -0300

    o alias da linha 11 no arquivo order service estava com um s a mais, o que impedia de conseguir fazer a busca corretamente

--- src/routers/order.router.ts
+++ src/routers/order.router.ts
@@ -3,7 +3,6 @@ import orderController from '../controllers/order.controller';
 
 const orderRouter = Router();
 
-// orderRouter.post('/orders', orderController.createOrder);
 orderRouter.get('/orders', orderController.listOrders);
 
 export default orderRouter;
--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,26 +1,14 @@
-// import OrderModel, { 
-//   OrderInputtableTypes,
-//   OrderSequelizeModel, 
-// } from '../database/models/order.model';
 import OrderModel, { 
   OrderSequelizeModel, 
 } from '../database/models/order.model';
 import sequelize from '../database/models/index';
 import ProductModel from '../database/models/product.model';
-// import { Order } from '../types/Order';
 import { ServiceResponse } from '../types/ServiceResponse';
-  
-// function validateParams({ userId }: OrderInputtableTypes): string | null {
-//   if (!userId) return 'userId is required';
-    
-//   /* Se o objeto for válido retorna nulo. */
-//   return null;
-// }
   
 const listOrders = async () : Promise<ServiceResponse<OrderSequelizeModel[]>> => {
   const orders = await OrderModel.findAll({
     include: [
-      { model: ProductModel, as: 'productsIds', attributes: [] },
+      { model: ProductModel, as: 'productIds', attributes: [] },
     ],
     attributes: [
       'id',
@@ -33,25 +21,6 @@ const listOrders = async () : Promise<ServiceResponse<OrderSequelizeModel[]>> =>
   return { status: 'SUCCESSFUL', data: orders };
 };
   
-// async function createOrder(
-//   order: OrderInputtableTypes,
-// ): Promise<ServiceResponse<Order>> {
-//   let responseService: ServiceResponse<Order>;
-//   const error = validateParams(order);
-  
-//   if (error) {
-//     responseService = { status: 'INVALID_DATA', data: { message: error } };
-//     return responseService;
-//   }
-  
-//   const newOrder = await OrderModel.create(order);
-  
-//   responseService = { status: 'SUCCESSFUL', data: newOrder.dataValues };
-  
-//   return responseService;
-// }
-
 export default {
   listOrders,
-//   createOrder,
 };
